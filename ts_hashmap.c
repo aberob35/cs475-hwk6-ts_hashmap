999aacd85b3b5cd63044f16f926cbba4914618fb
aberob35 <arobin@pugetsound.edu>
2023-03-26 02:06:43 -0700

    Update ts_hashmap.c

--- ts_hashmap.c
+++ ts_hashmap.c
@@ -5,6 +5,7 @@
 #include <string.h>
 #include "ts_hashmap.h"
 
+
 /**
  * Creates a new thread-safe hashmap. 
  *
@@ -13,7 +14,14 @@
  */
 ts_hashmap_t *initmap(int capacity) {
   // TODO
-  return NULL;
+ts_hashmap_t *data = (ts_hashmap_t*) malloc(sizeof(struct ts_hashmap_t) * capacity);  
+data->capacity = capacity;
+data->size = 0;
+data->table = (ts_entry_t**) malloc(sizeof(ts_entry_t*) * capacity);
+for (int i = 0; i < capacity; i++){
+    data->table[i] = NULL;
+  }
+return data;
 }
 
 /**
@@ -24,8 +32,23 @@ ts_hashmap_t *initmap(int capacity) {
  */
 int get(ts_hashmap_t *map, int key) {
   // TODO
+  int pos = ((unsigned int)key) % map->capacity;
+  ts_entry_t *entry = map->table[pos];
+
+  while(entry != NULL && entry->next != NULL){
+    if(entry->key == key){
+      return entry->value;
+    }
+    entry = entry->next;
+  }
+
+  if(entry != NULL && entry->key == key){
+    printf("The gotten value is: %d \n", entry->value);
+    return entry->value;
+  }
   return INT_MAX;
 }
+
 
 /**
  * Associates a value associated with a given key.
@@ -36,7 +59,28 @@ int get(ts_hashmap_t *map, int key) {
  */
 int put(ts_hashmap_t *map, int key, int value) {
   // TODO
-  return INT_MAX;
+  unsigned int pos = ((unsigned int)key) % map->capacity;
+  ts_entry_t **hashMap = map->table;
+  ts_entry_t *entry = hashMap[pos];
+
+  while(entry != NULL && entry->key != key){
+    entry = entry->next;
+  }
+
+  if(entry == NULL){
+    entry = (ts_entry_t*) malloc(sizeof(ts_entry_t));
+    entry->key = key;
+    entry->value = value;
+    entry->next = hashMap[pos];
+    hashMap[pos] = entry;
+    map->size += 1;
+    return INT_MAX;
+    
+  }else{
+    int temp = entry->value;
+    entry->value = value;
+    return temp;
+  }
 }
 
 /**
@@ -47,7 +91,30 @@ int put(ts_hashmap_t *map, int key, int value) {
  */
 int del(ts_hashmap_t *map, int key) {
   // TODO
-  return INT_MAX;
+  unsigned int pos = ((unsigned int)key) % map->capacity;
+  ts_entry_t **hashMap = map->table;
+  ts_entry_t *entry =  hashMap[pos];
+  ts_entry_t *entryNext;
+  
+
+  while(entry != NULL && entry->key != key){
+    entryNext = entry;
+    entry = entry->next;
+  }
+
+  if(entry == NULL){
+    return INT_MAX;
+  }else{
+    int temp = entry->value;
+    if(entryNext == NULL){
+      map->table[pos] = entry->next;
+    }else {
+      entryNext->next = entry->next;
+    }
+    free(entry);
+    map->size--;
+    return temp;
+  }
 }
 
 
@@ -74,3 +141,4 @@ void printmap(ts_hashmap_t *map) {
     printf("\n");
   }
 }
+
